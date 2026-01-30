1227d0bbba66c7259b2ca80cfc1604d25b705ea9
GrryXyz <greyvaldy16@gmail.com>
2026-01-31 07:43:38 +0800

    GuildConfig.js

--- src/models/GuildConfig.js
+++ src/models/GuildConfig.js
@@ -4,14 +4,21 @@ module.exports = model("GuildConfig", new Schema({
   guildId: String,
   language: { type: String, default: "id" },
 
+  // Security
   antiLink: { type: Boolean, default: false },
 
+  // welcome
   welcomeChannel: String,
+  welcomeText: String,
   rulesChannel: String,
 
-  welcomeText: {
-    type: String,
-    default: "👋 Welcome {user} ke **{server}**!"
-  }
-}));
+  // anti raid
+  antiRaid: { type: Boolean, default: true },
+  raidLimit: { type: Number, default: 5 }, // join
+  raidTime: { type: Number, default: 10 }, // detik
 
+  // verify
+  verifyEnabled: { type: Boolean, default: true },
+  verifyRole: String,
+  verifyChannel: String
+}));
